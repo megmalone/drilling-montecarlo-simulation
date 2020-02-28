c8fbf6c50ac4bda1cb876f30d46ce77940cfadb5
Meg Malone <margaretjeane@gmail.com>
2020-02-28 10:50:29 -0500

    updating phases 1, 2

--- phase1.R
+++ phase1.R
@@ -4,6 +4,7 @@ library(readxl)
 library(dplyr)
 library(lubridate)
 library(EnvStats)
+
 set.seed(1234)
 
 ###################
@@ -13,95 +14,221 @@ set.seed(1234)
 # I created a copy of the data file from Moodle and included only the second sheet, which is why I didn't need to indicate sheet here.
 data <- read_excel("Analysis_Data.xlsx")
 
+###############
+#### Costs ####
+###############
+
 # Filtering the data to include only observations 1991-2006 and the arithmetic return columns
-data <- data[,c(1,5:7)] %>%
+costs <- data[, c(1:4)] %>%
   filter(as.Date('1991-06-30') <= Date) %>%
   filter(Date < as.Date('2007-06-30'))
-names(data)[2:4] <- c('oil', 'gas', 'dry')
+names(costs)[2:4] <- c('oil', 'gas', 'dry')
 
 # Add column of averages to the dataframe
 # Mean wasn't cooperating, so I computed this the old-fashioned way!
-data$avg <- (data$oil + data$gas + data$dry) / 3
+costs$avg <- (costs$oil + costs$gas + costs$dry) / 3
 
-######################################################
-#### Normal Distribution Estimation for 2006-2012 ####
-######################################################
+#################
+#### Changes ####
+#################
 
-# Using the mean and std deviation from the raw data 1991-2006
-est <- rnorm(6, mean = mean(data$avg), sd = sd(data$avg))
+# Filtering the data to include only observations 1991-2006 and the arithmetic return columns
+changes <- data[, c(1, 5:7)] %>%
+  filter(as.Date('1991-06-30') <= Date) %>%
+  filter(Date < as.Date('2007-06-30'))
+names(changes)[2:4] <- c('oil', 'gas', 'dry')
 
-##################################################################
-#### Tri Distribution Estimations for 2013-2015 and 2016-2019 ####
-##################################################################
+# Add column of averages to the dataframe
+# Mean wasn't cooperating, so I computed this the old-fashioned way!
+changes$avg <- (changes$oil + changes$gas + changes$dry) / 3
 
-# From 2012 to 2015 costs tended to decrease on average by 9.17% per year with a maximum of 22% and minimum of 7%.
-est2 <- rtri(3, min = 0.07, max = 0.22, mode = 0.0917)
+#### Returns ####
+returns <- costs$avg*(1 + changes$avg)
 
-# From 2015 to 2019 costs tended to increase on average by 5% per year with a maximum of 6% and minimum of 2%.
-est3 <- rtri(4, min = 0.02, max = 0.06, mode = 0.05)
+#######################################
+#### Simulation Assuming Normality ####
+#######################################
 
-#########################
-#### Predicting 2020 ####
-#########################
+P2020 <- rep(1, 10000)
 
-#### With the assumption of normality for 2006-2012 ####
-hist_returns <- c(data$avg, est, est2, est3)
-
-# 2020 is forecasted to follow the same increase distribution as from 2015 to 2019.
-r <- rtri(n=10000, min = 0.02, max = 0.06, mode = 0.05)
-P0 <- 0
-P1 <- P0*(1+r)
-
-mean(P1)
-sd(P1)
-
-hist(P1, breaks=50, main='2020 Return Distribution', xlab='Final Value')
-abline(v = mean(P1), col = "red", lwd = 2)
-abline(v = mean(P1) + sd(P1), col = "red", lwd = 2, lty = 2)
-abline(v = mean(P1) - sd(P1), col = "red", lwd = 2, lty = 2)
-
-#####################################
-#### Kernal Estimation 2006-2012 ####
-#####################################
-
-Est.returns <- rkde(fhat=kde(data$avg, h=1), n=1000)
-hist(Est.returns, breaks=50, main='Estimated One Year Value Distribution', xlab='Final Value')
-mean(Est.returns) # 0.191158
-sd(Est.returns) # 1.035745
-
-qqnorm(data$avg)
-
-test = rep(1, 1000)
-for (j in 1:1000) {
-  # 2007-2012
+for (i in 1:10000) {
+  for (j in 1:6) {
+    # 2007-2012
+    
+    if (j == 1) {
+      P0 <- costs$avg[16]
+      r <-
+        rnorm(
+          n = 1,
+          mean = mean(changes$avg),
+          sd = sd(changes$avg)
+        )
+      P2 <- P0 * (1 + r) # 2007
+    }
+    else{
+      P1 <- P2
+      r <-
+        rnorm(
+          n = 1,
+          mean = mean(changes$avg),
+          sd = sd(changes$avg)
+        )
+      P2 <- P1 * (1 + r) # 2008-1012
+    }
+    
+  }
   
-  if (j == 1) {
-    P0 <- costs$avg[16]
-    r <- rkde(
-      fhat = kde(
-        all_changes,
-        h = 1
-      ),
-      n = 1)
-    P2 <- P0 * (1 + r) # 2007
+  for (k in 1:3) {
+    # 2013-2015
+    
+    if (k == 1) {
+      # From 2013 to 2015 costs tended to decrease on average by 9.17% per year with a maximum of 22% and minimum of 7%.
+      r <- rtri(1,
+                min = 0.07,
+                max = 0.22,
+                mode = 0.0917)
+      P4 <- P2 * (1 - r) # 2013
+    }
+    else{
+      P3 <- P4
+      # From 2013 to 2015 costs tended to decrease on average by 9.17% per year with a maximum of 22% and minimum of 7%.
+      r <- rtri(1,
+                min = 0.07,
+                max = 0.22,
+                mode = 0.0917)
+      P4 <- P3 * (1 - r) # 2014-2015
+    }
+    
   }
-  else{
-    P1 <- P2
-    r <- rkde(
-      fhat = kde(
-        all_changes,
-        h = 1
-      ),
-      n = 1)
-    P2 <- P1 * (1 + r) # 2008-1012
+  for (m in 1:5) {
+    # 2016-2020
+    
+    if (m == 1) {
+      # From 2015 to 2019 costs tended to increase on average by 5% per year with a maximum of 6% and minimum of 2%.
+      r <- rtri(1,
+                min = 0.02,
+                max = 0.06,
+                mode = 0.05)
+      P6 <- P4 * (1 + r) # 2016
+    }
+    else{
+      P5 <- P4
+      # From 2015 to 2019 costs tended to increase on average by 5% per year with a maximum of 6% and minimum of 2%.
+      # 2020 is forecasted to follow the same increase distribution as from 2015 to 2019.
+      r <- rtri(1,
+                min = 0.02,
+                max = 0.06,
+                mode = 0.05)
+      P6 <- P5 * (1 + r) #2018-2020
+    }
+    
   }
-  test[j] <- P2
+  
+  P2020[i] <- P6
+  
 }
 
-hist(test)
+quantile(P2020)
+mean(P2020) # 3955.332
+sd(P2020) # 1147.395
 
-density_all_changes <- density(all_changes, bw="SJ-ste")
-dac <- rkde(fhat=kde(all_changes, h=1), n=1)
-P0*(1+dac)
-hist(dac, breaks=50, main='Estimated One Year Value Distribution', xlab='Final Value')
+#### Graph ####
+hist(P2020, main = 'Histogram of 2020 Cost Simulation', xlab = "Cost (in thousands)", border = F, col = rgb(0.2,0.8,0.5,0.5))
 
+#################################################
+#### Simulation with Kernal Density Estimate ####
+#################################################
+
+# Previously the Price Analysis group has worked under the assumption that these arithmetic changes
+# from one year to the next from 2006 to 2012 follow a Normal distribution.
+# Use QQ-plots or formal tests to see if you agree.
+
+# Because of the direction in the next bullet point, I'm going to plot all of the arithmetic changes.
+# It doesn't make sense to plot the simulated values for 2007-2012 from above because they were selected with
+# rnorm and would, of course, be normal.
+all_changes <- c(changes$oil, changes$gas, changes$dry)
+all_costs <- c(costs$oil, costs$gas, costs$dry)
+all_returns <- all_changes*(1 + all_costs)
+qqnorm(all_changes) # Looks very normal to me!!
+
+# The Price Analysis group would also like you to build a kernel density estimate of the distribution of 
+# arithmetic changes using the 48 observations described above (1990 – 2006). Use this kernel density 
+# to simulate the changes from 2006 to 2012 as well.
+
+density(all_changes) # bw:0.06718
+
+P2020kde <- rep(1, 10000)
+
+for (i in 1:10000) {
+  for (j in 1:6) {
+    # 2007-2012
+    
+    if (j == 1) {
+      P0 <- costs$avg[16]
+      r <- rkde(fhat = kde(all_changes, h = 0.06718), n = 1)
+      P2 <- P0 * (1 + r) # 2007
+    }
+    else{
+      P1 <- P2
+      r <- rkde(fhat = kde(all_changes, h = 0.06718), n = 1)
+      P2 <- P1 * (1 + r) # 2008-1012
+    }
+    
+  }
+  
+  for (k in 1:3) {
+    # 2013-2015
+    
+    if (k == 1) {
+      # From 2013 to 2015 costs tended to decrease on average by 9.17% per year with a maximum of 22% and minimum of 7%.
+      r <- rtri(1,
+                min = 0.07,
+                max = 0.22,
+                mode = 0.0917)
+      P4 <- P2 * (1 - r) # 2013
+    }
+    else{
+      P3 <- P4
+      # From 2013 to 2015 costs tended to decrease on average by 9.17% per year with a maximum of 22% and minimum of 7%.
+      r <- rtri(1,
+                min = 0.07,
+                max = 0.22,
+                mode = 0.0917)
+      P4 <- P3 * (1 - r) # 2014-2015
+    }
+    
+  }
+  for (m in 1:5) {
+    # 2016-2020
+    
+    if (m == 1) {
+      # From 2015 to 2019 costs tended to increase on average by 5% per year with a maximum of 6% and minimum of 2%.
+      r <- rtri(1,
+                min = 0.02,
+                max = 0.06,
+                mode = 0.05)
+      P6 <- P4 * (1 + r) # 2016
+    }
+    else{
+      P5 <- P4
+      # From 2015 to 2019 costs tended to increase on average by 5% per year with a maximum of 6% and minimum of 2%.
+      # 2020 is forecasted to follow the same increase distribution as from 2015 to 2019.
+      r <- rtri(1,
+                min = 0.02,
+                max = 0.06,
+                mode = 0.05)
+      P6 <- P5 * (1 + r) #2018-2020
+    }
+    
+  }
+  
+  P2020kde[i] <- P6
+  
+}
+
+mean(P2020kde) # 3964.344
+sd(P2020kde) # 1698.779
+quantile(P2020kde)
+
+#### Graph ####
+hist(P2020kde, main = 'Histogram of 2020 Cost Simulation with Kernel Density Estimation', xlab = "Cost (in thousands)", border = F, col = rgb(0.2,0.8,0.5,0.5))
--- phase2.R
+++ phase2.R
@@ -4,8 +4,9 @@ library(readxl)
 library(dplyr)
 library(lubridate)
 library(EnvStats)
+library(triangle)
 
-set.seed(1234)
+set.seed(12345)
 
 ###################
 #### Data Prep ####
@@ -14,9 +15,7 @@ set.seed(1234)
 # I created a copy of the data file from Moodle and included only the second sheet, which is why I didn't need to indicate sheet here.
 data <- read_excel("Analysis_Data.xlsx")
 
-###############
 #### Costs ####
-###############
 
 # Filtering the data to include only observations 1991-2006 and the arithmetic return columns
 costs <- data[, c(1:4)] %>%
@@ -28,9 +27,7 @@ names(costs)[2:4] <- c('oil', 'gas', 'dry')
 # Mean wasn't cooperating, so I computed this the old-fashioned way!
 costs$avg <- (costs$oil + costs$gas + costs$dry) / 3
 
-#################
 #### Changes ####
-#################
 
 # Filtering the data to include only observations 1991-2006 and the arithmetic return columns
 changes <- data[, c(1, 5:7)] %>%
@@ -45,12 +42,9 @@ changes$avg <- (changes$oil + changes$gas + changes$dry) / 3
 #### Returns ####
 returns <- costs$avg*(1 + changes$avg)
 
-##############################
 #### Estimated Oil Prices ####
-##############################
-
 pred <- read_excel('Projection_Data.xlsx')
-pred_all <- append(pred$Low, pred$High)
+pred$mean = (pred$High + pred$Low)/2
 
 #########################
 #### For Correlation ####
@@ -102,19 +96,17 @@ for (i in 1:10000) {
   # $279,500. This will remain constant across the lifetime of a well, but would
   # potentially be different for different wells. These costs are incurred 
   # during Year 0 as well for drilling, but stop after Year 0 if the well is dry.
-  prof <- years*rtri(
-    1,
-    min = 172000,
-    max = 279500,
-    mode = 215000
-  )/1000
+  prof <- years*rtriangle(n = 1,
+                          a = 172000,
+                          b = 279000,
+                          c = 215000)/1000
   expenses <- lease + seismic + prof
   
   for (j in 1:6) {
     # 2007-2012
     
     if (j == 1) {
-      P0 <- costs$avg[16] + expenses
+      P0 <- costs$avg[16]
       r <-
         rnorm(
           n = 1,
@@ -141,19 +133,19 @@ for (i in 1:10000) {
     
     if (k == 1) {
       # From 2013 to 2015 costs tended to decrease on average by 9.17% per year with a maximum of 22% and minimum of 7%.
-      r <- rtri(1,
-                min = 0.07,
-                max = 0.22,
-                mode = 0.0917)
+      r <- rtriangle(n = 1,
+                     a = 0.07,
+                     b = 0.22,
+                     c = 0.0917)
       P4 <- P2 * (1 - r) # 2013
     }
     else{
       P3 <- P4
       # From 2013 to 2015 costs tended to decrease on average by 9.17% per year with a maximum of 22% and minimum of 7%.
-      r <- rtri(1,
-                min = 0.07,
-                max = 0.22,
-                mode = 0.0917)
+      r <- rtriangle(n = 1,
+                     a = 0.07,
+                     b = 0.22,
+                     c = 0.0917)
       P4 <- P3 * (1 - r) # 2014-2015
     }
     
@@ -163,20 +155,20 @@ for (i in 1:10000) {
     
     if (m == 1) {
       # From 2015 to 2019 costs tended to increase on average by 5% per year with a maximum of 6% and minimum of 2%.
-      r <- rtri(1,
-                min = 0.02,
-                max = 0.06,
-                mode = 0.05)
+      r <- rtriangle(n = 1,
+                     a = 0.02,
+                     b = 0.06,
+                     c = 0.05)
       P6 <- P4 * (1 + r) # 2016
     }
     else{
       P5 <- P4
       # From 2015 to 2019 costs tended to increase on average by 5% per year with a maximum of 6% and minimum of 2%.
       # 2020 is forecasted to follow the same increase distribution as from 2015 to 2019.
-      r <- rtri(1,
-                min = 0.02,
-                max = 0.06,
-                mode = 0.05)
+      r <- rtriangle(n = 1,
+                     a = 0.02,
+                     b = 0.06,
+                     c = 0.05)
       P6 <- P5 * (1 + r) #2018-2020
     }
     
@@ -192,11 +184,10 @@ hist(P2020) # Cost of a dry well in 2020
 ##################################################
 #### Simulation Assuming Normality (Wet Well) ####
 ##################################################
-
 npv_dist <- rep(1, 10000)
 
 wells <- 1 # No. of wells
-years <- 15
+years <- 15 # Years being simulated
 
 for (i in 1:10000) {
   
@@ -205,7 +196,7 @@ for (i in 1:10000) {
     # 2007-2012
     
     if (j == 1) {
-      P0 <- costs$avg[16] + expenses
+      P0 <- costs$avg[16]
       r <-
         rnorm(
           n = 1,
@@ -232,19 +223,19 @@ for (i in 1:10000) {
     
     if (k == 1) {
       # From 2013 to 2015 costs tended to decrease on average by 9.17% per year with a maximum of 22% and minimum of 7%.
-      r <- rtri(1,
-                min = 0.07,
-                max = 0.22,
-                mode = 0.0917)
+      r <- rtriangle(n = 1,
+                     a = 0.07,
+                     b = 0.22,
+                     c = 0.0917)
       P4 <- P2 * (1 - r) # 2013
     }
     else{
       P3 <- P4
       # From 2013 to 2015 costs tended to decrease on average by 9.17% per year with a maximum of 22% and minimum of 7%.
-      r <- rtri(1,
-                min = 0.07,
-                max = 0.22,
-                mode = 0.0917)
+      r <- rtriangle(n = 1,
+                     a = 0.07,
+                     b = 0.22,
+                     c = 0.0917)
       P4 <- P3 * (1 - r) # 2014-2015
     }
     
@@ -254,24 +245,26 @@ for (i in 1:10000) {
     
     if (m == 1) {
       # From 2015 to 2019 costs tended to increase on average by 5% per year with a maximum of 6% and minimum of 2%.
-      r <- rtri(1,
-                min = 0.02,
-                max = 0.06,
-                mode = 0.05)
+      r <- rtriangle(n = 1,
+                     a = 0.02,
+                     b = 0.06,
+                     c = 0.05)
       P6 <- P4 * (1 + r) # 2016
     }
     else{
       P5 <- P4
       # From 2015 to 2019 costs tended to increase on average by 5% per year with a maximum of 6% and minimum of 2%.
       # 2020 is forecasted to follow the same increase distribution as from 2015 to 2019.
-      r <- rtri(1,
-                min = 0.02,
-                max = 0.06,
-                mode = 0.05)
-      P6 <- P5 * (1 + r) #2018-2020
+      r <- rtriangle(n = 1,
+                     a = 0.02,
+                     b = 0.06,
+                     c = 0.05)
+      P6 <- (P5 * (1 + r))*1000 #2018-2020
     }
     
   }
+  
+  drill_cost <- P6
   
   ############### Now that we have a distibution of the drilling cost. We need to simulate additional expenses. ##############
 
@@ -283,7 +276,8 @@ for (i in 1:10000) {
     n = 1,
     mean = 600*wells,
     sd = 50*wells
-  )/1000
+  )
+  
   # The number of seismic sections per well is Normally distributed with a mean
   # of 3 sections and a standard deviation of 0.35 per well (lower number of wells
   # means lower number of sections to purchase data on); the seismic costs per 
@@ -292,7 +286,8 @@ for (i in 1:10000) {
     n = 1,
     mean = 3*wells,
     sd = 0.35*wells
-  )/1000
+  )
+  
   # If it is determined that there is oil present in the reservoir 
   # (and we have not drilled a dry hole), engineers must prepare the
   # well to produce oil at the optimum sustainable rates. For this 
@@ -303,49 +298,57 @@ for (i in 1:10000) {
     mean = 390000,
     sd = 50000
   )
-  expenses <- lease + seismic + complete
+  
+  expenses <- lease + seismic + complete + drill_cost
   
   ############ Next, we have to predict the rate of oil production and # of barrels produced per year. ###################
   
   # The initial production rate tested from the drilled well. This is the rate the oil is produced at Year 1.
   # The IP’s follow a Lognormal distribution with a mean of 420 BOPD and a standard deviation of 120 BOPD. 
   ip <- rlnorm(
-    n = 1,
-    meanlog = 420,
-    sdlog = 120
+    n = 10000,
+    meanlog = 6,
+    sdlog = 0.28
   )
+  
   # A declining production rate that describes the annual decrease in production from the beginning of the year to
   # the end of the same year. To simplify the problem, we will assume each well has the same decline rate for every year of its life, 
   # but this could be different across wells.
   # The rate of decline is Uniformly distributed between 15 and 32 percent.
   decline <- runif(
-    n = 1,
+    n = 10000,
     min = 0.15,
     max = 0.32
   )
   
   # We incorporate an additional constraint in the production model; we have imposed acorrelation coefficient of 0.64 
   # between the IP and the decline rate assumptions that are drawn from their respective distributions during each trial of the simulation.
-  R <- matrix(data=cbind(1, 0.64, 0.64, 1), nrow=2)
+  R <- matrix(data = cbind(1, 0.64, 0.64, 1), nrow = 2)
   U <- t(chol(R))
-  ip_decline <- cbind(standardize(ip), standardize(decline))
-
-  co_ip_decline <- U %*% t(ip_decline)
-  co_ip_decline <- t(co_ip_decline)
-
-  final_ip_decline <- cbind(destandardize(co_ip_decline[,2], ip))
   
-  for(y in 1:years){
-    revenue = rep(1, years)
+  Both.r <- cbind(standardize(decline), standardize(ip))
+  ip_decline.r <- U %*% t(Both.r)
+  ip_decline.r <- t(ip_decline.r)
+  
+  final.ip_decline.r <- cbind(destandardize(ip_decline.r[,1], decline),
+                      destandardize(ip_decline.r[,2], ip))
+  
+  summary(final.ip_decline.r)
+  
+  ############## Knowing these figures, we can estimate revenue from sales minus operational and preofessional costs #############
+  
+  revenue <- rep(1, years)
+  wacc <- rep(1, years)
+  for (y in 1:years){
     
-    # decline <- final_ip_decline[i,2]
-    # ip <- final_ip_decline[i,1]
+    decline <- final.ip_decline.r[i,1]
+    ip <- final.ip_decline.r[i,2]
     
-    ppb <- rtri(
-      1,
-      min = min(pred_all),
-      max = max(pred_all),
-      mode = mean(pred_all)
+    ppb <- rtriangle( # Price per barrel
+      n = 1,
+      a = pred$Low[y],
+      b = pred$High[y],
+      c = pred$mean[y]
     )
     
     if (y == 1){
@@ -370,6 +373,7 @@ for (i in 1:10000) {
       mean = 0.75,
       sd = 0.02
     )
+    
     # State taxes levied on produced oil and gas are assumed to be a constant value of 4.6% of revenue. Taxes are applied after the NRI.
     income <- sales*nri - (0.046*sales)
     
@@ -383,7 +387,7 @@ for (i in 1:10000) {
       mean = 2.25,
       sd = 0.3
     )
-    opcost = barrels*op
+    opcost <- barrels*op
     
     # The project team annual costs in salary and benefits per well depends on 
     # the time that the project team spends on the well. We believe the salary 
@@ -392,23 +396,26 @@ for (i in 1:10000) {
     # $279,500. This will remain constant across the lifetime of a well, but would
     # potentially be different for different wells. These costs are incurred 
     # during Year 0 as well for drilling, but stop after Year 0 if the well is dry.
-    prof <- rtri(
-      1,
-      min = 172000,
-      max = 279500,
-      mode = 215000
-    )/1000
-    revenue
+    prof <- rtriangle(n = 1,
+                      a = 172000,
+                      b = 279000,
+                      c = 215000)
+    
     revenue[y] <- income - opcost - prof
+    
+    wacc[y] <- (1.1)**y
   }
   
-  sum_revenue = rep(0, years)
-  for (r in revenue){
-    m <- revenue[r]/1+1.1*(P6)
+  sum_revenue <- rep(1, years)
+  for (x in 1:years){
+    sum_revenue[x] <- revenue[x]/wacc[x]
   }
+  
   npv <- -expenses + sum(sum_revenue)
+  npv_dist[i] <- (npv/1000000)
   
-  npv_dist[i] <-npv
 }
 
-hist(npv_dist)
+hist(npv_dist) # These values are in millions
+median(npv_dist)
+
